8b1692cd652cfd3819e6479f9528e53c8cf14d7d
Ryan Hoegg <ryan.hoegg@gmail.com>
2014-11-01 20:42:02 -0500

    Complete transformation requirement

--- hoegg-sample.js
+++ hoegg-sample.js
@@ -1,16 +1,19 @@
 angular.module("hoegg-sample", [])
 	.controller("WordController", function($scope, $http, $interval) {
+		function beginWaiting(message) {
+			$scope.data.word = null;
+			$scope.data.disabled = true;
+			$scope.data.status = message;
+			$scope.data.statusPromise = $interval(function() {
+				$scope.data.status += ".";
+			}, 1000);
+		}
+
 		$scope.data = {};
 		$scope.data.status = 'Well?';
 		$scope.data.disabled = false;
 
 		$scope.generateWord = function() {
-			$scope.data.word = null;
-			$scope.data.disabled = true;
-			$scope.data.status = 'Please wait for word.'
-			$scope.data.statusPromise = $interval(function() {
-				$scope.data.status += ".";
-			}, 1000);
 			$http.get('http://oseberg.io/interview/word_generator.php')
 				.success(function(data, status, headers, config) {
 					$scope.data.word = data;
@@ -20,7 +23,24 @@ angular.module("hoegg-sample", [])
 				.error(function(data, status, headers, config) {
 					$scope.data.disabled = false;
 					$interval.cancel($scope.data.statusPromise);
+					$scope.data.status = 'Oops!';
 				});
-		}
+				beginWaiting('Please wait for word.');
+		};
+		
+		$scope.transformWord = function() {
+			$http.get('http://oseberg.io/interview/shifter.php', { params: { word: $scope.data.word } })
+				.success(function(data, status, headers, config) {
+					$scope.data.word = data;
+					$scope.data.disabled = false;
+					$interval.cancel($scope.data.statusPromise);
+				})
+				.error(function(data, status, headers, config) {
+					$scope.data.disabled = false;
+					$interval.cancel($scope.data.statusPromise);
+					$scope.data.status = 'Oops!';
+				});
+			beginWaiting('Please wait for transformation.');
+		};
 	});
 	
