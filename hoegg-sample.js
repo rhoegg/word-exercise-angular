062e101180852ec15680406eac20c458a8d9d5d4
Ryan Hoegg <ryan.hoegg@gmail.com>
2014-11-01 21:05:29 -0500

    added some error handling, disabled buttons while waiting. Something like 2 hours invested so far

--- hoegg-sample.js
+++ hoegg-sample.js
@@ -1,8 +1,10 @@
 angular.module("hoegg-sample", [])
 	.controller("WordController", function($scope, $http, $interval) {
 		function beginWaiting(message) {
+			$scope.data.previousWord = $scope.data.word;
 			$scope.data.word = null;
 			$scope.data.disabled = true;
+			$scope.data.serverError = false;
 			$scope.data.status = message;
 			$scope.data.statusPromise = $interval(function() {
 				$scope.data.status += ".";
@@ -17,29 +19,34 @@ angular.module("hoegg-sample", [])
 		$scope.data = {};
 		$scope.data.status = 'Well?';
 		$scope.data.disabled = false;
+		$scope.data.serverError = false;
 
 		$scope.generateWord = function() {
+			if ($scope.data.disabled) return;
 			$http.get('http://oseberg.io/interview/word_generator.php')
 				.success(function(data) {
 					$scope.data.word = data;
 					stopWaiting();
 				})
-				.error(function(data, status, headers, config) {
+				.error(function(data) {
 					stopWaiting();
-					$scope.data.status = 'Oops!';
+					$scope.data.serverError = data;
+					$scope.data.word = $scope.data.previousWord;
 				});
 				beginWaiting('Please wait for word.');
 		};
 		
 		$scope.transformWord = function() {
+			if ($scope.data.disabled) return;
 			$http.get('http://oseberg.io/interview/shifter.php', { params: { word: $scope.data.word } })
 				.success(function(data) {
 					$scope.data.word = data;
 					stopWaiting();
 				})
-				.error(function(data, status, headers, config) {
+				.error(function(data) {
 					stopWaiting();
-					$scope.data.status = 'Oops!';
+					$scope.data.serverError = data;
+					$scope.data.word = $scope.data.previousWord;
 				});
 			beginWaiting('Please wait for transformation.');
 		};
